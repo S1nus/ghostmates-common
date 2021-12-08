38535a88b58008b8bda44e8e2b5e2faa4284d98c
Connor O'Hara <connor@switchboard.xyz>
2021-12-08 17:07:13 -0500

    changing this to use with the new repos

--- src/lib.rs
+++ src/lib.rs
@@ -1,49 +1,42 @@
-use async_std::{
-    net::{TcpStream}
-};
-
-use futures_codec::{Bytes, BytesMut, LengthCodec, Framed, FramedRead, FramedWrite, Decoder, Encoder};
-use std::io::{Error, ErrorKind};
-use async_std::io::{BufWriter, BufReader};
-
-pub type StringCodecReader<'a> = FramedRead<BufReader<&'a TcpStream>, StringCodec>;
-pub type StringCodecWriter<'a> = FramedWrite<BufWriter<&'a TcpStream>, StringCodec>;
-
-pub struct StringCodec(pub LengthCodec);
-
-pub fn new_codec_reader(stream: BufReader<&TcpStream>) -> FramedRead<BufReader<&TcpStream>, StringCodec> {
-    FramedRead::new(stream, StringCodec(LengthCodec))
+#[derive(Serialize, Deserialize, Debug)]
+pub enum GhostmatesMessage {
+    Identify {
+        ghostmates_address: String,
+        pubkey: SodiumPublicKey,
+    },
+    SuccesfulIdentify,
+    FailedIdentify,
+    SuccesfulLookup {
+        pubkey: SodiumPublicKey,
+        ghostmates_address: String,
+    },
+    FailedLookup {
+        ghostmates_address: String,
+    },
+    Lookup {
+        dest_address: String,
+    },
+    DirectMessage {
+        dest_address: String,
+        encrypted_message: Vec<u8>,
+        nonce: SodiumNonce
+    },
+    IncomingMessage {
+        from_address: String,
+        encrypted_message: Vec<u8>,
+        nonce: SodiumNonce,
+    },
 }
 
-pub mod message_types;
-
-pub fn new_codec_writer(stream: BufWriter<&TcpStream>) -> FramedWrite<BufWriter<&TcpStream>, StringCodec> {
-    FramedWrite::new(stream, StringCodec(LengthCodec))
-}
-
-impl Encoder for StringCodec {
-    type Item = String;
-    type Error = Error;
-
-    fn encode(&mut self, src: Self::Item, dst: &mut BytesMut) -> Result<(), Self::Error> {
-        let bytes = Bytes::from(src);
-        self.0.encode(bytes, dst)
+#[derive(Serialize, Deserialize, Debug)]
+pub enum PCheckMessage {
+    RequestForPCheck {
+        recipient_ghost_address: String
     }
 }
 
-impl Decoder for StringCodec {
-    type Item = String;
-    type Error = Error;
-
-    fn decode(&mut self, src: &mut BytesMut) -> Result<Option<Self::Item>, Self::Error> {
-        match self.0.decode(src)? {
-            Some(bytes) => {
-                match String::from_utf8(bytes.to_vec()) {
-                    Ok(string) => Ok(Some(string)),
-                    Err(e) => Err(Error::new(ErrorKind::InvalidData, e))
-                }
-            },
-            None => Ok(None),
-        }
-    }
+#[derive(Serialize, Deserialize, Debug)]
+pub enum ProtocolMessage {
+    ACheck,
+    PCheck(PCheckMessage),
 }
