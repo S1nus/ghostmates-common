0ca18315843bb48f9e2d82db623c4ecf28040a78
Connor O'Hara <connor@switchboard.xyz>
2021-12-10 02:21:53 -0500

    paillier crate doesn't have serialize for the ciphertexts. bummer. i'll have to try using BigInt

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,12 @@ use serde::{Serialize, Deserialize};
 use sodiumoxide::crypto::box_::{PublicKey as SodiumPublicKey, SecretKey as SodiumSecretKey, Nonce as SodiumNonce};
 use sodiumoxide::crypto::box_;
 
+use paillier::*;
+use paillier::{
+    EncryptionKey as PaillierEncryptionKey,
+    encoding::EncodedCiphertext as PaillierEncodedCiphertext
+};
+
 #[derive(Serialize, Deserialize, Debug)]
 pub enum GhostmatesMessage {
     Identify {
@@ -37,7 +43,19 @@ pub enum PCheckMessage {
     RequestForPCheck {
         recipient_ghost_address: String,
         courier_ghost_address: String
-    }
+    },
+    /*RecipientToSenderRound1 {
+        paillier_key: PaillierEncryptionKey,
+        enc_ab_pairs: [(PaillierEncodedCiphertext<usize>, PaillierEncodedCiphertext<usize>); 128],
+        a_shares: [PaillierEncodedCiphertext<usize>; 128],
+        b_shares: [PaillierEncodedCiphertext<usize>; 128],
+    },
+    CourierToSenderRound1 {
+        paillier_key: PaillierEncryptionKey,
+        enc_ab_pairs: [(PaillierEncodedCiphertext<usize>, PaillierEncodedCiphertext<usize>); 128],
+        a_shares: [PaillierEncodedCiphertext<usize>; 128],
+        b_shares: [PaillierEncodedCiphertext<usize>; 128]
+    }*/
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
